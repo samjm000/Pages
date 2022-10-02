4a0e8f1f532eb747aeffe7b2a4b28de58008ec25
Sam McInerney <samjm000@yahoo.co.uk>
2022-10-02 05:31:54 +0100

    initial commit

---
+++ pages/tests.py
@@ -0,0 +1,14 @@
+from urllib import response
+from django.test import SimpleTestCase, TestCase
+
+# Create your tests here.
+class HomePageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+
+class AboutPageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
